f5aa29a7f56b21b99bb8876ef5677ff40ce3197a
amitambitions <amit.ambitions@gmail.com>
2017-05-15 22:36:52 -0700

    added first version implementation.
    Need to use different STL containers to see how that affects the programming simplicity.

--- Week4/dijkstra/dijkstra.cpp
+++ Week4/dijkstra/dijkstra.cpp
@@ -6,10 +6,57 @@ using std::vector;
 using std::queue;
 using std::pair;
 using std::priority_queue;
+using std::numeric_limits;
 
+// Check that we are making a heap and using the node for our heap algorithm. When we 
+// choose source vertex, then we need to see which is the next vertex to be promoted to
+// the root, so we need to know its index as well as the weight.
+struct Node {
+  int index, distance;
+  Node(int index = 0, int distance = 0): index(index), distance(distance) {}
+};
+
+// By default priority_queue keeps the value with highest value as the root i.e. it makes
+// max-heap and we need a min-heap. So we just make a comparator to reverse the order.
+struct cmp {
+	bool operator()(Node first, Node second){
+		return first.distance > second.distance;
+	}
+};
+
+// Ideally this program gives the cost matrix separately but that is waste of cycles it seems.
+// TODO - 
+// 1. merge into single vector
+// 2. refresh about std::pairs as that could simplify the solution here.
 int distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
-  //write your code her
-  return -1;
+  //write your code here
+  vector<int> dist(adj.size(), numeric_limits<int>::max());
+  dist[s] = 0;
+  
+  // Custom priority queue.
+  priority_queue<Node, vector<Node>, cmp> pq;
+  
+  // push the source vertex.
+  pq.push(Node(s, dist[s]));
+  while(!pq.empty()) {
+	Node u = pq.top();
+	pq.pop();
+	int u_index = u.index;
+	
+	// Relax each edge associated with given node.
+    for (int i = 0; i < adj[u_index].size(); i++) {
+	  int v = adj[u_index][i];
+	  if(dist[v] > dist[u_index] + cost[u_index][i]) {
+	    dist[v] = dist[u_index] + cost[u_index][i];
+        pq.push(Node(v, dist[v])); 
+	  }
+	}	
+  }
+  
+  if(dist[t] == numeric_limits<int>::max())
+    return -1;
+    
+  return dist[t];
 }
 
 int main() {
