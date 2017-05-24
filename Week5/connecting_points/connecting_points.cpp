4122962568d9943024f20505cfd08bde5b97b07e
amitambitions <amit.ambitions@gmail.com>
2017-05-23 20:56:43 -0700

    Add files via upload

--- Week5/connecting_points/connecting_points.cpp
+++ Week5/connecting_points/connecting_points.cpp
@@ -6,6 +6,22 @@
 
 using std::vector;
 
+// The algorithm is very simple, but first we need to understand the following things:
+//
+// 1. Some points are given in the plane, with their x and y coordinates. The graph is not
+// given directly, so we need to assume that the graph is given implicitly.
+//
+// 2. We need to connect using minimum cable, so it means that all the nodes are reachable
+// from each other and then we need to find the minimum spanning tree.
+//
+// 3. Weights of each edge is also not given directly, we need to apply the simple formula
+// and calculate the weight of each edge.
+//
+// So as the preprocessing step, we need to create the graph with all the edges having the
+// weights.
+//
+// Next we can apply for kruskal algorithm using rank, which needs the notion of a node
+// having a rank and a parent.
 double minimum_distance(vector<int> x, vector<int> y) {
   double result = 0.;
   //write your code here
